521e3cb99558a35317f6faef0f5a13f1f272b112
ravidur112-coder <ravidur112@gmail.com>
2026-01-16 13:34:39 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,8 +6,8 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "",
-ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/DANUWA-MD/DANUWA-MD/blob/main/images/DANUWA-MD.png?raw=true",
-ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 DANUWA-MD Is Alive Now😍*",
+ALIVE_IMG: process.env.ALIVE_IMG || "Images/eea947509079e243a378b4c39d3de22f.jpg",
+ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 DARK SOUL-MD Is Alive Now😍*",
 BOT_OWNER: '94776121326',  // Replace with the owner's phone number
 
 
